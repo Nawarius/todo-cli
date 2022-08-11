bbcfc9cb84c70bf7925f5966897c2c85017a66b4
Artem Ermakov <nawarius1993@gmail.com>
2022-08-11 17:30:31 +0400

    Adds: Read init state of todo from file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::io::Read;
 use std::{env, fs, io};
 use std::collections::HashMap;
+use std::str::FromStr;
 
 struct Todo {
     list_map: HashMap<String, bool>
 }
 impl Todo {
+    fn new () -> Result<Todo, io::Error> {
+        let mut file = fs::OpenOptions::new().write(true).create(true).read(true).open("db.txt")?;
+        let mut content = String::new();
+
+        file.read_to_string(&mut content)?;
+        let list_map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.split(" -> ").collect::<Vec<&str>>())
+            .map(|vec| (String::from(vec[0]), bool::from_str(vec[1]).unwrap()))
+            .collect();
+
+        Ok(Todo { list_map })
+    }
+
     fn add (&mut self, key: String) {
         self.list_map.insert(key, true);
     }
@@ -22,7 +38,7 @@ fn main() {
     let action = env::args().nth(1).expect("Need some action");
     let item = env::args().nth(2).expect("Need some item");
 
-    let mut todo = Todo { list_map: HashMap::new() };
+    let mut todo = Todo::new().unwrap();
 
     if action == "add" {
         todo.add(item);
@@ -30,8 +46,8 @@ fn main() {
         match todo.save() {
             Ok(_) => println!("Success save"),
             Err(_) => println!("Error save")
-        }
+        };
 
-    }
+    };
     
 }
