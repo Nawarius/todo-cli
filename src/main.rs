9a599cec458751e593e43dfb9562209ebfbe13a2
Artem Ermakov <nawarius1993@gmail.com>
2022-08-14 19:20:54 +0400

    Adds: Add fltk (toml), Rewrited all todo logic for using with gui interface

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,101 @@
 use std::{env, fs, io};
 use std::collections::HashMap;
+use fltk::{app, prelude::*, window::Window, button::Button, frame, group, input};
+use std::str::FromStr;
 
+#[derive(Debug, Clone)]
 struct Todo {
-    list_map: HashMap<String, bool>
+    list_map: Vec<String>
 }
 impl Todo {
     fn new () -> Result<Todo, io::Error> {
         let file = fs::OpenOptions::new().write(true).create(true).read(true).open("db.json")?;
-        //let mut content = String::new();
-
-        // file.read_to_string(&mut content)?;
-        // let list_map: HashMap<String, bool> = content
-        //     .lines()
-        //     .map(|line| line.split(" -> ").collect::<Vec<&str>>())
-        //     .map(|vec| (String::from(vec[0]), bool::from_str(vec[1]).unwrap()))
-        //     .collect();
 
         match serde_json::from_reader(file) {
-            Ok(list_map) => Ok(Todo { list_map }),
-            Err(e) if e.is_eof() => Ok(Todo { list_map: HashMap::new() }),
+            Ok(list_map) => {
+                //let list_map: HashMap<String, bool> = list_map;
+                //let list_map: Vec<String> = list_map.iter().map(|(key, val)| format!("{} -> {}", key, val)).collect();
+                Ok(Todo { list_map })
+            },
+            Err(e) if e.is_eof() => Ok(Todo { list_map: vec![] }),
             Err(e) => panic!("An error occurred: {}", e)
         }
-        
     }
 
-    fn add_task (&mut self, key: String) {
-        self.list_map.insert(key, true);
-    }
-    fn save (&self) -> Result<(), Box<dyn std::error::Error>> {
-        let f = std::fs::OpenOptions::new().write(true).create(true).open("db.json")?;
-        
-        serde_json::to_writer_pretty(f, &self.list_map)?;
-        Ok(())
-
-        // let mut result = String::new();
-
-        // for (key, val) in self.list_map.iter() {
-        //     let record = format!("{} -> {}\n", key, val);
-        //     result.push_str(&record);
-        // }
-        // fs::write("db.txt", result)
-    }
-    fn done_task (&mut self, key: &String) -> Option<()> {
-        match self.list_map.get_mut(key) {
-            Some(val) => Some(*val = false),
-            None => None
+    fn add_task (&mut self, key: String) -> Option<()> {
+        if let Some(_) = self.list_map.iter().find(|el| el.contains(&key)) { None } 
+        else {
+            let task = format!("{} -> {}", key, true);
+            self.list_map.push(task);
+            Some(())
         }
     }
-}
-fn main() {
-    let action = env::args().nth(1).expect("Need some action");
-    let item = env::args().nth(2).expect("Need some item");
 
-    let mut todo = Todo::new().unwrap();
-
-    if action == "add" {
-        todo.add_task(item.clone());
-
-        match todo.save() {
-            Ok(_) => println!("Success save"),
-            Err(_) => println!("Error save")
-        };
-
-    } else if action == "done" {
-        match todo.done_task(&item) {
-            Some(_) => match todo.save() {
-                Ok(_) => println!("Success save"),
-                Err(_) => println!("Error save")
-            },
-            None => println!("The task completed unsuccessful")
-        };
+    fn save (&self) -> Result<(), Box<dyn std::error::Error>> {
+        let f = std::fs::OpenOptions::new().write(true).create(true).read(true).open("db.json")?;
+        serde_json::to_writer_pretty(f, &self.list_map)?;
+        Ok(())
     }
+
+    fn done_task (&mut self, key: &String) {
+        if let Some(task) = self.list_map.iter_mut().find(|el| el.contains(key)) {
+            println!("Success done -->{}<--", key);
+            *task = format!("{} -> false", key);
+            println!("{:?}", self.list_map);
+            self.save();
+        } 
+    }
+
+    fn reset_task (&mut self, key: &String) {
+        if let Some(task) = self.list_map.iter_mut().find(|el| el.contains(key)) {
+            println!("Success reset -->{}<--", key);
+            *task = format!("{} -> true", key);
+            println!("{:?}", self.list_map);
+            self.save();
+        } 
+    }
+}
+static mut todo: Todo = Todo { list_map: vec![]};
+
+fn main() {
+    unsafe { todo = Todo::new().unwrap(); }
+
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 800, 600, "Hello from rust");
+    let mut _frame = frame::Frame::default();
+
+    let mut offset = 40;
+
+unsafe {
+    for task in todo.list_map.iter() {
+        let task = task.split(" -> ").collect::<Vec<&str>>()[0];
+
+        let flex = group::Flex::default().with_size(800, 30).with_pos(0, offset).row();
+
+        let inp = input::Input::default().with_size(300, 50).set_value(task);
+        let mut reset_btn = Button::default().with_size(45, 50).with_label("Reset");
+        let mut done_btn = Button::default().with_label("Done");
+        let mut remove_btn = Button::default().with_size(45, 50).with_label("Remove");
+
+        done_btn.set_callback(|_| { 
+            todo.done_task(&task.to_string());
+        });
+        reset_btn.set_callback(|_| { 
+            todo.reset_task(&task.to_string());
+        });
+
+        flex.end();
+
+        offset += 50;
+    }
+}
     
+    wind.end();
+    wind.show();
+    
+    
+
+    app.run().unwrap();
+    
+    println!("End")
 }
