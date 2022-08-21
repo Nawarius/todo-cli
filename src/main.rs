8e9de8e369137d9da9d999e03a016d8dc694d793
Artem Ermakov <nawarius1993@gmail.com>
2022-08-21 20:29:16 +0400

    Adds: Ability add tasks from gui interface

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{fs, io};
-use fltk::{app, prelude::*, window::Window, button::Button, frame::Frame, group, input};
+use fltk::{app, prelude::*, window::Window, button::Button, frame::Frame, group::Flex, input::Input};
 use serde_json;
 
 #[derive(Debug, Clone)]
@@ -55,6 +55,8 @@ static mut todo: Todo = Todo { list_map: vec![]};
 enum TaskMessage {
     Done(&'static str),
     Reset(&'static str),
+    Add(),
+    Remove(&'static str)
 }
 struct ToDoApp {
     app: app::App,
@@ -62,11 +64,11 @@ struct ToDoApp {
     receiver: app::Receiver<TaskMessage>,
     sender: app::Sender<TaskMessage>,
     list_map: Vec<String>,
-    frame_map: Vec<(String, Frame)>
+    frame_map: Vec<(String, Frame)>,
+    add_input: Input
 }
 
 static mut TaskVec: Vec<String> = vec![];
-static mut FrameVec: Vec<(String, Frame)> = vec![];
 
 impl ToDoApp {
     fn read_todo_drom_db () -> Result<Vec<String>, io::Error> {
@@ -91,46 +93,76 @@ impl ToDoApp {
         } 
     }
 
+    fn add_task (&mut self, task_name: &str) {
+        if let None = self.list_map.iter().find(|el| el.contains(task_name)) { 
+            let task = format!("{} -> Not Done", task_name);
+            self.list_map.push(task);
+        } 
+    }
+
     fn new () -> Self {
         let list_map = ToDoApp::read_todo_drom_db().unwrap();
         
         let app = app::App::default();
         let (sender, receiver) = app::channel();
         let window = Window::default().with_size(800, 600).with_label("App");
-        
+
         Self {
             app,
             window,
             sender,
             receiver,
             list_map,
-            frame_map: vec![]
+            frame_map: vec![],
+            add_input: Input::default(),
         }
     }
 
+    fn clear_window (&mut self) {
+        self.frame_map = vec![];
+
+        self.window.clear();
+        self.window.redraw();
+    }
+
     unsafe fn view (&mut self) {
+        self.clear_window();
+
         TaskVec = self.list_map.iter().map(|s| s.to_string()).collect::<Vec<String>>();
 
         let mut offset = 40;
+
+        self.window.begin();
+
+        let flex_add = Flex::default().with_size(800, 30).with_pos(0, 0).row();
+
+            let add_input = Input::default().with_size(200, 30);
+            self.add_input = add_input;
+            
+            let mut add_btn = Button::default().with_size(100, 30).with_label("Add task");
+            add_btn.emit(self.sender, TaskMessage::Add());
+
+        flex_add.end();
 
         for task in TaskVec.iter() {
             let vec: Vec<&str> = task.split(" -> ").collect();
             let task = vec[0];
             let res = vec[1];
 
-            let flex = group::Flex::default().with_size(800, 30).with_pos(0, offset).row();
+            let flex = Flex::default().with_size(800, 30).with_pos(0, offset).row();
 
-            let task_name = Frame::default().with_label(task);
-            let task_res = Frame::default().with_label(res);
+                let task_name = Frame::default().with_label(task);
+                let task_res = Frame::default().with_label(res);
 
-            self.frame_map.push((task.to_string(), task_res));
+                self.frame_map.push((task.to_string(), task_res));
 
-            let mut reset_btn = Button::default().with_size(45, 50).with_label("Reset task");
-            let mut done_btn = Button::default().with_label("Done task");
-            let mut remove_btn = Button::default().with_size(45, 50).with_label("Remove");
+                let mut reset_btn = Button::default().with_size(45, 50).with_label("Reset task");
+                let mut done_btn = Button::default().with_label("Done task");
+                let mut remove_btn = Button::default().with_size(45, 50).with_label("Remove");
 
-            done_btn.emit(self.sender, TaskMessage::Done(task));
-            reset_btn.emit(self.sender, TaskMessage::Reset(task));
+                done_btn.emit(self.sender, TaskMessage::Done(task));
+                reset_btn.emit(self.sender, TaskMessage::Reset(task));
+                remove_btn.emit(self.sender, TaskMessage::Remove(task));
 
             flex.end();
 
@@ -138,9 +170,10 @@ impl ToDoApp {
         }
         self.window.end();
         self.window.show();
+
     }
 
-    pub fn run(&mut self) {
+    unsafe fn run(&mut self) {
         while self.app.wait() {
             if let Some(msg) = self.receiver.recv() {
                 match msg {
@@ -159,7 +192,17 @@ impl ToDoApp {
                             let task_name = task_name.to_string();
                             self.mark_task(&task_name, "Not Done");
                         } 
-                    }
+                    },
+                    TaskMessage::Add() => {
+                        let task_name = self.add_input.value();
+                        self.add_task(&task_name);
+
+                        self.add_input.set_value("");
+                        self.view();
+                    },
+                    TaskMessage::Remove(key) => {
+                        println!("{key}");
+                    },
                 }
             }
         }
